c976ee4d4913c899a1138574073d8922ed3f5100
Omotola Bolarin <113185763+omotolab@users.noreply.github.com>
2022-11-29 19:36:01 +0000

    Add Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+    srcDir: "xyz",
+    css: ['uikit/dist/css/uikit.min.css']
+    /* modules: [
+        'nuxt-windicss'
+    ], */
 })

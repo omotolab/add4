0c823718190a9db38646cf134fe2a06ebcda5499
-.- <bolarin@omoto.la>
2022-11-29 23:47:15 +0100

    Update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     srcDir: "xyz",
-    css: ['uikit/dist/css/uikit.min.css']
-    /* modules: [
+    css: ['uikit/dist/css/uikit.min.css'],
+    modules: [
         'nuxt-windicss'
-    ], */
+    ]
 })
 

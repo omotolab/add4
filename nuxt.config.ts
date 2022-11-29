f99ac445fb24c2f0ef27433610cb271534d56634
Omotola Bolarin <113185763+omotolab@users.noreply.github.com>
2022-11-29 21:11:24 +0000

    Unvercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    nitro: {
-        preset: "vercel",
-    },
     srcDir: "xyz",
     css: ['uikit/dist/css/uikit.min.css']
     /* modules: [

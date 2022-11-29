cea110579e6d371d68d8cc56988ab74268a7f34e
Omotola Bolarin <113185763+omotolab@users.noreply.github.com>
2022-11-29 19:59:30 +0000

    Update Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+    nitro: {
+        preset: "vercel",
+    },
     srcDir: "xyz",
     css: ['uikit/dist/css/uikit.min.css']
     /* modules: [

a15bef3cc9d043f09ec6ef69c0cf12edaeefaa9a
-.- <bolarin@omoto.la>
2022-12-19 10:47:56 +0100

    Update app.vue

--- src/app.vue
+++ src/app.vue
@@ -1,59 +1,65 @@
-<script setup>
-useHead({
+<script setup async >
+import { useStorage } from '@vueuse/core'
+
+/* useHead({
   link: [
     {
-      rel: 'icon', type: 'image/png', href: '/nuxt.png',
+      rel: 'icon', type: 'image/png', href: '/images/logo.png',
+    },
+    {
+      rel: 'manifest', href: '/manifest.json',
     },
   ],
+}) */
+
+const config = useRuntimeConfig()
+const { width, height } = useWindowSize()
+const languages = usePreferredLanguages()
+const { language, isSupported } = useNavigatorLanguage()
+const touch = ref(null)
+const data = await useStorage('data', {})
+console.log('data_2', data.value)
+onMounted(async () => {
+    console.log('data_1', await data.value)
 })
 </script>
 <template>
-    <cover class="flex justify-center items-center h-screen w-screen ">
-        <div class="uk-container">
-            <div class="uk-flex uk-flex-around">
-                <div class="uk-card uk-card-small uk-background-muted uk-border-rounded uk-width-large">
-                    <div class="uk-width-1-1">
-                        <div class="uk-card-header uk-card-hover">
-                            <div class="uk-flex uk-flex-between">
-                                <div>Title</div>
-                                <div>
-                                    <a href="#hello" uk-toggle="target: #mini" type="button">
-                                        <span uk-icon="expand"></span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <hr class="uk-padding-remove uk-margin-remove">
-                        <div class="tm uk-card-body uk-card-small">
-                            <div class="uk-width-1-1 uk-height-small">
-                                <pre contenteditable="true" class=" editable">Hello</pre>
-                                <ul class="uk-text-small uk-iconnav uk-margin-small-top">
-                                    <li>
-                                        <a class="uk-icon-button uk-width-auto uk-border-rounded">
-                                            Send
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <environment v-if="false" :window="{
+        height,
+        width
+    }">
+        <!-- <div class="container">
+            <div class="p-2" v-if="touch">touched: {{ touch._custom }}</div>
+            <div class="p-2">{{  focused  }}</div>
+            <div class="p-2">{{ language }}</div>
+            <div class="p-2">{{ languages }}</div>
+            <div class="p-2">dashboard</div>
+            <div class="p-2">document</div>
+            <div>
+                <div class="uk-form-label p-2">{{ $t('welcome') }}</div>
+                <select class="uk-select" v-model="locale" @change="handleChange">
+                    <option v-for="loc in locales" :value="loc.code">{{ loc.name }}</option>
+                </select>
             </div>
-        </div>
-
-        <!-- This is the modal -->
-        <div id="mini" class="uk-modal-full" uk-modal>
-            <div class="uk-modal-dialog">
-                <button class="uk-modal-close-default" type="button" uk-close></button>
-                <div class="uk-modal-body" uk-height-viewport="offset-top; true; offset-bottom: true">
-                    <div>Hello Content</div>
-                </div>
-                <div class="uk-modal-footer">
-                    <div>Hello Footer</div>
-                </div>
+            <div>
+                <div class="uk-form-label p-2">{{ $t('welcome') }}</div>
+                <select class="uk-select" v-model="voice" @change="handleChange">
+                    <option v-for="voice in choices" :value="voice">{{ voice.name }}</option>
+                </select>
             </div>
+
+            <div class="p-2">{{ message }}</div>
+            <div class="p-2">
+                <block-form />
+            </div>
+            
+        </div> -->
+    </environment>
+    <client-only>
+        <div class="uk-flex uk-flex-center uk-section">
+            <document-editor v-model="data" />
         </div>
+    </client-only>
 
 
-    </cover>
 </template>
